87708e45ecdee034453b9c4c84e2bc52afd8c257
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-09-22 15:49:06 +0700

    Menambahkan file notification.xml dan modified MyFirebaseMessagingService.kt

--- app/src/main/java/com/nurkholiq/firebase_cloud_messaging_basic/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/nurkholiq/firebase_cloud_messaging_basic/MyFirebaseMessagingService.kt
@@ -1,4 +1,22 @@
 package com.nurkholiq.firebase_cloud_messaging_basic
 
-class MyFirebaseMessagingService {
+import android.app.PendingIntent
+import android.content.Intent
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    // generate the notification
+    // attach the notification created with the custom layout
+    // show the notification
+
+    fun generateNotification(title: String, message: String){
+
+        val intent = Intent (this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT)
+
+    }
+
 }
